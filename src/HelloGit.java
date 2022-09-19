b4d0c430c8f6d8072dbd48914edacdf6458a54e1
blekanic <s52116@edu.campus02.at>
2022-09-19 19:24:06 +0200

    Added Matrikelnummer and fixed typo in Comment

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,7 +1,7 @@
 public class HelloGit {
-    // main + TAB oder pvsm + TAB
+    // main + TAB oder psvm + TAB
     public static void main(String[] args) {
         System.out.println("Hello Git"); // sout + TAB
-
+        System.out.println("Meine Matrikelnummer ist 123456");
     }
 }
